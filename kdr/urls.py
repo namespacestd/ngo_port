636d11fbbe6ef5702103defefeab9b5df191652e
Alex Dong <ihasnamespacestd@gmail.com>
2013-09-16 21:28:03 -0400

    Initalized repository

---
+++ kdr/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+from kdr.views import *
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+	url(r'^$', index),
+	url (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
+    # Examples:
+    # url(r'^$', 'kdr.views.home', name='home'),
+    # url(r'^kdr/', include('kdr.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+urlpatterns += staticfiles_urlpatterns()
